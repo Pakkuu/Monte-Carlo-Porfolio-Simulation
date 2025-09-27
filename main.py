51a585275402d1a00a4a52a34aed0fcfb20c76c6
pakkuu <ethan.pvarghese@gmail.com>
2025-09-27 16:41:56 -0500

    added monte carlo sim logic

--- main.py
+++ main.py
@@ -22,4 +22,26 @@ meanReturns, covMatrix = get_data(stocks, startDate, endDate)
 weights = np.random.random(len(meanReturns))
 weights /= np.sum(weights)
 
-print("Weights:", weights)
+# Monte Carlo Simulation
+num_simulations = 10
+num_days = 252
+
+mean_matrix = np.full(shape=(num_days, len(weights)), fill_value=meanReturns)
+mean_matrix = mean_matrix.T
+
+portfolio_simulations = np.full(shape=(num_days, num_simulations), fill_value=0.0)
+print(portfolio_simulations)
+
+initial_portfolio_value = 10000
+
+for i in range(num_simulations):
+    Z = np.random.normal(size=(num_days, len(weights)))
+    L = np.linalg.cholesky(covMatrix)
+    daily_returns = mean_matrix + np.inner(L, Z)
+    portfolio_simulations[:, i] = np.cumprod(np.inner(weights, daily_returns.T) + 1) * initial_portfolio_value
+    
+plt.plot(portfolio_simulations)
+plt.ylabel('Portfolio Value ($)')
+plt.xlabel('Days')
+plt.title('Monte Carlo Simulation of Portfolio Value Over One Year')
+plt.show()
